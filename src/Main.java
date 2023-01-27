1e7007ece3f4c2dd634403d461f60e68df557331
TIME <opout777@yandex.ru>
2023-01-27 23:55:55 +0300

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+    public static boolean leapYear(int yearCheck) {
+        return (yearCheck % 4 == 0 && yearCheck % 100 != 0) || yearCheck % 400 == 0;
+    }
+    public static void task1() {
+        System.out.println("Задание 1");
+        int year = 2021;
+        boolean leapYearCheck = leapYear(year);
+        if (leapYearCheck) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
