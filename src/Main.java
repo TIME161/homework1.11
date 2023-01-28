584be1de1ba5a735ce2557483d5e1a759e4487ab
TIME <opout777@yandex.ru>
2023-01-28 09:50:34 +0300

    Task 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
     public static boolean leapYear(int yearCheck) {
         return (yearCheck % 4 == 0 && yearCheck % 100 != 0) || yearCheck % 400 == 0;
@@ -22,6 +23,24 @@ public class Main {
             return "";
         }
     }
+    public static String deliveryDays(int deliveryDistance) {
+        int days = 0;
+        if (deliveryDistance < 20) {
+            days = 1;
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            days = 2;
+        } else if (deliveryDistance < 100 && deliveryDistance >= 60) {
+            days = 3;
+        } else if (deliveryDistance > 100) {
+            days = 4;
+        }
+        boolean deliveryPossibility = days < 4;
+        if (deliveryPossibility) {
+            return ("Потребуется дней: " + days);
+        } else {
+            return "Доставки нет";
+        }
+        }
     public static void task1() {
         System.out.println("Задание 1");
         int year = 2021;
@@ -37,5 +56,9 @@ public class Main {
         int deviceYear = 2023;
         int deviceSystem = 0;
         System.out.println("Установите " + yearCheck(deviceYear) + "версию приложения для " + deviceOs(deviceSystem) + " по ссылке");
+    }
+    public static void task3() {
+        int distance = 95;
+        System.out.println(deliveryDays(distance));
     }
 }
